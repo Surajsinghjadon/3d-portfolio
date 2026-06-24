f3cce54f7f54db4a44289e5d6d9bfcf9609ba716
Suraj Singh Jadon <surajsinghrajput5040@gmail.com>
2026-06-25 02:44:13 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,13 +46,13 @@ const Navbar = () => {
           AM
         </a>
         <a
-          href="https://www.linkedin.com/in/akashrmalhotra/"
+          href="https://www.linkedin.com/in/suraj-singh-jadon-bb320a274"
           className="navbar-connect"
           data-cursor="disable"
           target="_blank"
           rel="noreferrer"
         >
-          linkedin.com/in/akashrmalhotra
+          https://www.linkedin.com/in/suraj-singh-jadon-bb320a274
         </a>
         <ul>
           <li>
