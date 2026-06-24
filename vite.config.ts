79b96f588d96fcb15d7764fba0f1093e464b4faa
Suraj Singh Jadon <surajsinghrajput5040@gmail.com>
2026-06-24 23:29:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // GitHub Pages par website chalane ke liye sahi path
+  base: '/Portfolio/', // Sateek sub-folder mapping for your exact link
 })
